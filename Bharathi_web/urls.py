38de0e4be2e2589d742dad508a71e10b66e5ae00
Surya123450 <94666339+Surya123450@users.noreply.github.com>
2023-03-28 07:51:54 +0530

    update

--- Bharathi_web/urls.py
+++ Bharathi_web/urls.py
@@ -4,7 +4,7 @@ from Bharathi_web import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path('', views.index),
+    path('base/', views.base),
     path('', views.index1),
     path('chapter/', views.chapter1,name='chapter'),
     path('about/', views.about,name='about'),
--- Bharathi_web/views.py
+++ Bharathi_web/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
 
 def index(request):
     return render(request, 'Bharathi_web/index.html')
 
+def base(request):
+    return render(request, 'Bharathi_web/base.html')
+
 def index1(request):
-    return render(request, 'Bharathi_web/index1.html')
+    message = "Hi "
+    name=["Surya","prakash","srini"]
+    time = datetime.datetime.now()
+    hour = int(time.strftime('%H'))
+    if hour<12:
+        message+="Good Moring"
+    else:
+        message+="Good Evening"
+    dic = {"name":name,'time':time,'message':message}
+    return render(request, 'Bharathi_web/index1.html',context=dic,)
 
 def chapter1(request):
     return render(request, 'Bharathi_web/chapter1.html')
